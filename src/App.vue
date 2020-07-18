5e474eb8401d98dd43b3cb3aa0cdbda519ba95ae
Walle0606 <michelle210181@gmail.com>
2020-07-19 02:13:44 +0800

    add search feature

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,37 @@
 <template>
   <v-app>
-    <v-card max-width="500" class="wrapper">
+    <v-card max-width="500" class="wrapper  mx-auto">
       <v-toolbar color="indigo" dark>
         <v-toolbar-title>Todo List</v-toolbar-title>
         <v-spacer></v-spacer>
-        <v-btn icon>
+        <v-text-field
+          clear-icon
+          rounded
+          solo
+          dense
+          light
+          v-model="inputWord"
+          label="找東西.."
+          @input="findMatch(inputWord)"
+          v-show="visible"
+          class="mt-6 resize"
+        ></v-text-field>
+
+        <v-btn v-if="visible" icon @click="clear">
+          <v-icon>mdi-close</v-icon>
+        </v-btn>
+        <v-btn v-else icon @click="visible = !visible">
           <v-icon>mdi-magnify</v-icon>
         </v-btn>
       </v-toolbar>
       <v-list>
         <v-list-item>
           <v-list-item-content class="mr-5">
-            <v-text-field label="要做些什麼？" v-model="name"></v-text-field>
+            <v-text-field
+              label="要做些什麼？"
+              v-model="name"
+              @keyup.enter="createList(name)"
+            ></v-text-field>
           </v-list-item-content>
           <v-list-item-icon>
             <v-btn
@@ -26,33 +46,39 @@
             </v-btn>
           </v-list-item-icon>
         </v-list-item>
-
-        <v-list-item v-for="list in allList" :key="list.id">
-          <v-list-item-icon>
-            <v-btn
-              icon
-              v-if="list.done === true"
-              @click="updateList(list.id, list.name, list.done)"
-            >
-              <v-icon>mdi-checkbox-marked</v-icon></v-btn
-            >
-            <v-btn
-              icon
-              v-else
-              @click="updateList(list.id, list.name, list.done)"
-            >
-              <v-icon>mdi-checkbox-blank-outline</v-icon></v-btn
-            >
-          </v-list-item-icon>
-          <v-list-item-content>
-            <v-list-item-title v-text="list.name"></v-list-item-title>
-          </v-list-item-content>
-          <v-list-item-icon>
-            <v-btn icon @click="deleteList(list.id)">
-              <v-icon color="red">mdi-trash-can-outline</v-icon></v-btn
-            >
-          </v-list-item-icon>
-        </v-list-item>
+        <template>
+          <v-list-item v-if="displayedList.length < 1">
+            <v-list-item-content>
+              <v-list-item-title>沒有待辦事項唷！</v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+          <v-list-item v-for="list in displayedList" :key="list.id">
+            <v-list-item-icon>
+              <v-btn
+                icon
+                v-if="list.done === true"
+                @click="updateList(list.id, list.name, list.done)"
+              >
+                <v-icon>mdi-checkbox-marked</v-icon></v-btn
+              >
+              <v-btn
+                icon
+                v-else
+                @click="updateList(list.id, list.name, list.done)"
+              >
+                <v-icon>mdi-checkbox-blank-outline</v-icon></v-btn
+              >
+            </v-list-item-icon>
+            <v-list-item-content>
+              <v-list-item-title v-text="list.name"></v-list-item-title>
+            </v-list-item-content>
+            <v-list-item-icon>
+              <v-btn icon @click="deleteList(list.id)">
+                <v-icon color="red">mdi-trash-can-outline</v-icon></v-btn
+              >
+            </v-list-item-icon>
+          </v-list-item>
+        </template>
       </v-list>
     </v-card>
   </v-app>
@@ -67,26 +93,31 @@ export default {
     return {
       name: '',
       done: false,
-      allList: [],
+      displayedList: [],
       search: '',
+      staticArr: [],
+      inputWord: '',
+      visible: false,
     }
   },
   mounted() {
-    this.readList()
+    this.readList('init')
   },
   methods: {
-    async readList() {
+    async readList(con) {
       try {
-        let arr = []
+        this.staticArr = []
         const querySnapshot = await db.collection('listItem').get()
         querySnapshot.forEach((doc) => {
-          arr.push({
+          this.staticArr.push({
             id: doc.id,
             name: doc.data().name,
             done: doc.data().done,
           })
         })
-        this.allList = Object.assign([], { ...this.allList, ...arr })
+        if (con === 'create' || con === 'init') {
+          this.displayedList = Object.assign([], this.staticArr)
+        }
       } catch (error) {
         Toast.fire({
           type: 'error',
@@ -99,7 +130,8 @@ export default {
         if (name !== '') {
           await db.collection('listItem').add({ name: name, done: false })
           this.name = ''
-          this.readList()
+          this.displayedList.push({ name: name, done: false })
+          this.readList('create')
         }
       } catch (error) {
         Toast.fire({
@@ -114,10 +146,12 @@ export default {
           .collection('listItem')
           .doc(id)
           .update({
-            name: name,
             done: !done,
           })
           .then(() => {
+            let delIndex = this.displayedList.map((e) => e.id).indexOf(id)
+            this.displayedList[delIndex].done = !this.displayedList[delIndex]
+              .done
             this.readList()
           })
       } catch (error) {
@@ -134,8 +168,8 @@ export default {
           .doc(id)
           .delete()
           .then(() => {
-            let delIndex = this.allList.map((e) => e.id).indexOf(id)
-            this.allList.splice(delIndex, 1)
+            let delIndex = this.displayedList.map((e) => e.id).indexOf(id)
+            this.displayedList.splice(delIndex, 1)
             this.readList()
           })
       } catch (error) {
@@ -145,6 +179,20 @@ export default {
         })
       }
     },
+    findMatch(str) {
+      if (str.trim() !== '') {
+        this.displayedList = this.staticArr.filter((item) =>
+          item.name.match(str)
+        )
+      } else {
+        this.displayedList = JSON.parse(JSON.stringify(this.staticArr))
+      }
+    },
+    clear() {
+      this.inputWord = ''
+      this.visible = !this.clear
+      this.readList()
+    },
   },
 }
 </script>
@@ -152,6 +200,10 @@ export default {
 <style lang="scss" scoped>
 .wrapper {
   margin: 50px auto;
-  width: 400px;
+  width: 350px;
+}
+
+.resize {
+  width: 140px !important;
 }
 </style>
